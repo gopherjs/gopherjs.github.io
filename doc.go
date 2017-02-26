c78e3939e3fd77cc9d5bed5e296d8c8a11fa5291
Richard Musiol <mail@richard-musiol.de>
2017-02-26 19:10:39 +0100

    replace broken pages with simple redirect to GitHub repo

--- doc.go
+++
@@ -1,6 +0,0 @@
-// Package gopherjswebsite is the www.gopherjs.org website source.
-//
-// It should be updated with `go generate github.com/gopherjs/gopherjs.github.io/...`.
-package gopherjswebsite
-
-//go:generate go run generate.go
