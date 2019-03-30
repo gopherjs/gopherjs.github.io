1682bc3da0e943dd4672550f452e8407aaf5d930
Dmitri Shuralyov <dmitri@shuralyov.com>
2019-03-30 03:33:28 -0400

    playground/internal/imports: add Go 1.12 identifiers
    Regenerated with:
    
    	go generate github.com/gopherjs/gopherjs.github.io/playground/internal/imports

--- playground/internal/imports/mkstdlib.go
+++ playground/internal/imports/mkstdlib.go
@@ -68,6 +68,7 @@ func main() {
 		mustOpen(api("go1.9.txt")),
 		mustOpen(api("go1.10.txt")),
 		mustOpen(api("go1.11.txt")),
+		mustOpen(api("go1.12.txt")),
 		mustOpen("gopherjs.txt"),
 	)
 	sc := bufio.NewScanner(f)
