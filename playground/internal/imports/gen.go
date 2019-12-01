0aede165a0e2bf1c24d34d06c1d02dedd9dc53e1
Dmitri Shuralyov <dmitri@shuralyov.com>
2019-12-01 00:38:10 -0500

    playground/internal/imports: add syscall/js identifiers
    GopherJS 1.12-2 has added support for syscall/js package,
    so it should be available as part of imports.
    
    The syscall/js API is not included in the GOROOT/api/go1.*.txt files
    at this time, and so it needs to be added to mkstdlib.go explicitly.
    Use the mkapi.go program to determine the syscall/js API.
    
    Regenerate zstdlib.go with the updated generator, using Go 1.12.13:
    
    	go1.12.13 generate github.com/gopherjs/gopherjs.github.io/playground/internal/imports
    
    Updates golang/go#27590

--- playground/internal/imports/gen.go
+++ playground/internal/imports/gen.go
@@ -1,5 +1,6 @@
+//go:generate go run mkapi.go -output=syscalljs.txt -contexts js-wasm syscall/js
 //go:generate go run mkapi.go -output=gopherjs.txt -gopath github.com/gopherjs/gopherjs/js
 //go:generate go run mkstdlib.go -output=zstdlib
-//go:generate rm gopherjs.txt
+//go:generate rm syscalljs.txt gopherjs.txt
 
 package imports
